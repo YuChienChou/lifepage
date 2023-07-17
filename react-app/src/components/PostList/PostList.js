702c6045856b3e6b8943688b0bdcd8539edeeed1
Leslie Chou <chouyuchien@gmail.com>
2023-07-17 09:58:55 -0700

    completed PostList component and rendered in both userHome and useProfile page

---
+++ react-app/src/components/PostList/PostList.js
@@ -0,0 +1,60 @@
+import ReactPlayer from 'react-player/youtube';
+import { Link } from 'react-router-dom';
+import OpenModalButton from '../OpenModalButton';
+import Comment from '../Comments/Comment';
+import EditDeleteCommentModal from '../Comments/EditDeleteCommentModal';
+import EditDeletePostModal from '../EditDeletePost/EditDeletePostModal';
+
+
+
+export default function PostList({sessionUser, post}) {
+    
+    return (
+        <>
+
+        <li key={post.id} className='post-list'>
+            {/* {console.log("post in the for loop: ", post)} */}
+            <div id='post-list-div'>
+            
+                    <div id='user-img-name'>
+                        <div id='img-and-link'>
+                            <Link to={`/user/${post.User.id}`}> <img src={post.User.profile_picture} alt={post.User.first_name} /></Link>
+                            <Link to={`/user/${post.User.id}`}>{post.User.firstname} {post.User.lastname}</Link>
+                        </div>
+
+                        {post.User.id === sessionUser.id ? 
+                            <div id='edit-post-div'>
+                                <OpenModalButton
+                                    buttonText={<i className="fa-solid fa-ellipsis"></i>}
+                                    modalComponent={<EditDeletePostModal sessionUser={sessionUser} post={post}/>}
+                                />
+                            </div> : null 
+                        }
+                    </div>
+                    
+                <div id='post-content'>
+                    <div id='content'>
+                        <p>{post.title}</p>
+                        <p>{post.body}</p>
+                    </div>
+                    {post.img? 
+                    <div id='img-post'>
+                    <img src={post.img} alt=""/></div>
+                    : null
+                    }  
+                    {post.video ? 
+                        <div id='video-post'>
+                            <ReactPlayer url={post.video} controls width='100%' height='100%'/>
+
+                        </div> : null
+                
+                    }
+                </div>
+                
+            </div>
+                <Comment sessionUser={sessionUser} post={post} />
+                    
+        </li>
+        </>
+    )
+}
--- react-app/src/components/User/userHome.js
+++ react-app/src/components/User/userHome.js
@@ -8,7 +8,7 @@ import OpenModalButton from '../OpenModalButton';
 import CreatePost from '../CreatePost/CreatePostModal';
 import Comment from '../Comments/Comment';
 import EditDeletePostModal from '../EditDeletePost/EditDeletePostModal';
-import PostList from '../Comments/PostList/PostList';
+import PostList from '../PostList/PostList';
 
 import './user.css'
 
@@ -54,52 +54,52 @@ export default function UserHome() {
                 <div id='post-list-container'>
                     <ul>
                         {postsArr.reverse().map((post) => (
-                            // <PostList sessionUser={sessionUser} post={post} />
+                            <PostList sessionUser={sessionUser} post={post} />
                         // console.log("post in the for loop: ", post)
                         
-                            <li key={post.id} className='post-list'>
-                                {/* {console.log("post in the for loop: ", post)} */}
-                                <div id='post-list-div'>
+                            // <li key={post.id} className='post-list'>
+                            //     {/* {console.log("post in the for loop: ", post)} */}
+                            //     <div id='post-list-div'>
                                 
-                                        <div id='user-img-name'>
-                                            <div id='img-and-link'>
-                                                <Link to={`/user/${post.User.id}`}> <img src={post.User.profile_picture} alt={post.User.first_name} /></Link>
-                                                <Link to={`/user/${post.User.id}`}>{post.User.firstname} {post.User.lastname}</Link>
-                                            </div>
+                            //             <div id='user-img-name'>
+                            //                 <div id='img-and-link'>
+                            //                     <Link to={`/user/${post.User.id}`}> <img src={post.User.profile_picture} alt={post.User.first_name} /></Link>
+                            //                     <Link to={`/user/${post.User.id}`}>{post.User.firstname} {post.User.lastname}</Link>
+                            //                 </div>
 
-                                            {post.User.id === sessionUser.id ? 
-                                                <div id='edit-post-div'>
-                                                    <OpenModalButton
-                                                        buttonText={<i className="fa-solid fa-ellipsis"></i>}
-                                                        modalComponent={<EditDeletePostModal sessionUser={sessionUser} post={post}/>}
-                                                    />
-                                                </div> : null 
-                                            }
-                                        </div>
+                            //                 {post.User.id === sessionUser.id ? 
+                            //                     <div id='edit-post-div'>
+                            //                         <OpenModalButton
+                            //                             buttonText={<i className="fa-solid fa-ellipsis"></i>}
+                            //                             modalComponent={<EditDeletePostModal sessionUser={sessionUser} post={post}/>}
+                            //                         />
+                            //                     </div> : null 
+                            //                 }
+                            //             </div>
                                      
-                                    <div id='post-content'>
-                                        <div id='content'>
-                                            <p>{post.title}</p>
-                                            <p>{post.body}</p>
-                                        </div>
-                                        {post.img? 
-                                        <div id='img-post'>
-                                        <img src={post.img} alt=""/></div>
-                                        : null
-                                        }  
-                                        {post.video ? 
-                                            <div id='video-post'>
-                                                <ReactPlayer url={post.video} controls width='100%' height='100%'/>
+                            //         <div id='post-content'>
+                            //             <div id='content'>
+                            //                 <p>{post.title}</p>
+                            //                 <p>{post.body}</p>
+                            //             </div>
+                            //             {post.img? 
+                            //             <div id='img-post'>
+                            //             <img src={post.img} alt=""/></div>
+                            //             : null
+                            //             }  
+                            //             {post.video ? 
+                            //                 <div id='video-post'>
+                            //                     <ReactPlayer url={post.video} controls width='100%' height='100%'/>
 
-                                            </div> : null
+                            //                 </div> : null
                                     
-                                        }
-                                    </div>
+                            //             }
+                            //         </div>
                                   
-                                </div>
-                                    <Comment sessionUser={sessionUser} post={post} />
+                            //     </div>
+                            //         <Comment sessionUser={sessionUser} post={post} />
                                         
-                            </li>
+                            // </li>
                         
                         ))}
                     </ul>
