f271998406ec75e8ff5c545275d57bf7ae213ad9
Leslie Chou <chouyuchien@gmail.com>
2023-07-19 16:10:23 -0700

    modifying seeder files

--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -4,7 +4,6 @@ from datetime import date
 
 def seed_posts():
     post1 = Post(
-        # title = 'Introduction to Python',
         img = 'https://devblogs.microsoft.com/python/wp-content/uploads/sites/12/2018/08/pythonfeature.png',
         video = '',
         body = 'Python is a versatile programming language.',
@@ -13,8 +12,7 @@ def seed_posts():
         updated_at =  date.today(),
     )
     post2 = Post(
-        # title = 'Beautiful Landscapes',
-        img = 'https://images.unsplash.com/photo-1619994948937-ef1e758d46ca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1333&q=80',
+        img = 'https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg',
         video = '',
         body = 'Check out this breathtaking landscapes from a remote mountain.',
         user_id =  2,
@@ -22,17 +20,72 @@ def seed_posts():
         updated_at =  date.today(),
     )
     post3 = Post(
-        # title = 'How to gain control of your free time',
-        img = 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+        img = '',
         video = 'https://www.youtube.com/watch?v=n3kNlFMXslo&t=62s&ab_channel=TED',
         body = 'Learn how to manage your time more efficiently and achieve your goals.',
         user_id =  3,
         created_at =  date.today(),
         updated_at =  date.today(),
     )
+    post4 = Post (
+        img = 'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg',
+        video = '',
+        body = 'Discover some mouth-watering recipes that are easy to make at home.',
+        user_id = ,
+        created_at = date.today(),
+        updated_at = date.today(),
+    )
+    post5 = Post (
+        img = ,
+        video = ,
+        body = ,
+        user_id = ,
+        created_at = date.today(),
+        updated_at = date.today(),
+    )
+    post6 = Post (
+        img = ,
+        video = ,
+        body = ,
+        user_id = ,
+        created_at = date.today(),
+        updated_at = date.today(),
+    )
+    post7 = Post (
+        img = ,
+        video = ,
+        body = ,
+        user_id = ,
+        created_at = date.today(),
+        updated_at = date.today(),
+    )
+    post8 = Post (
+        img = ,
+        video = ,
+        body = ,
+        user_id = ,
+        created_at = date.today(),
+        updated_at = date.today(),
+    )
+    post9 = Post (
+        img = ,
+        video = ,
+        body = ,
+        user_id = ,
+        created_at = date.today(),
+        updated_at = date.today(),
+    )
+    post10 = Post (
+        img = ,
+        video = ,
+        body = ,
+        user_id = ,
+        created_at = date.today(),
+        updated_at = date.today(),
+    )
     
 
-    post_list = [post1, post2, post3]
+    post_list = [post1, post2, post3, post4, post5, post6, post7, post8, post9, post10]
     db.session.add_all(post_list)
     db.session.commit()
     print("Posts seeded to db")
