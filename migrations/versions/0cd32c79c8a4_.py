99906ba6ad403d4efebbdbdfba2428c1be216f45
Leslie Chou <chouyuchien@gmail.com>
2023-07-24 17:37:56 -0700

    modified env.py and migration file for redeployment

--- migrations/versions/0cd32c79c8a4_.py
+++ migrations/versions/0cd32c79c8a4_.py
@@ -8,6 +8,9 @@ Create Date: 2023-07-19 20:11:51.735868
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '0cd32c79c8a4'
@@ -58,6 +61,11 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE posts SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE comments SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
