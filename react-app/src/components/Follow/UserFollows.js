c6ef4414b7d68ba4fac197b3a11e81648135f5bd
Leslie Chou <chouyuchien@gmail.com>
2023-08-03 19:55:59 -0700

    completed user follow styling and implementation

--- react-app/src/components/Follow/UserFollows.js
+++ react-app/src/components/Follow/UserFollows.js
@@ -1,13 +1,13 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { addUserFollowsThunk, deleteUserFollowsThunk, getUserFollowsThunk } from "../../store/user";
-
+import './userfollows.css';
 
 export default function UserFollows({sessionUser, followedUserId}) {
     const userFollows = useSelector((state) => state.users.userFollows);
-    console.log("user follows in create Follow component: ", userFollows);
+    // console.log("user follows in create Follow component: ", userFollows);
     const userFollowsArr = Object.values(userFollows);
-    console.log("user follows array in create follow component: ", userFollowsArr);
+    // console.log("user follows array in create follow component: ", userFollowsArr);
     const [showFunDiv, setShowFunDiv] = useState(false);
     const dispatch = useDispatch();
 
@@ -53,15 +53,12 @@ export default function UserFollows({sessionUser, followedUserId}) {
     return (
         <>
         <div id='add-user-follow-div'>
-            <i className="fa-solid fa-ellipsis" onClick={showFun}></i>
-            <div id='add-user-follow-fun'>
-
-                {res.includes(followedUserId) ? 
-                    <p onClick={UserFollowFun}>Following</p>
-                    : <p onClick={UserFollowFun}>Follow</p>
-                
-                }       
-            </div>
+            {/* <i className="fa-solid fa-ellipsis" onClick={showFun}></i> */}
+            {res.includes(followedUserId) ? 
+                <p id='following' onClick={UserFollowFun}>Following</p>      
+                : 
+                <p id='not-follow'onClick={UserFollowFun}>Follow</p>      
+            }
         </div>
 
         </>
