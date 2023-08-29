4fbf690970d2bd867bb71ce61b72e1978cef7a86
Leslie Chou <chouyuchien@gmail.com>
2023-08-28 22:34:03 -0700

    fixed user friend list bug

--- react-app/src/components/Friend/userFriends.js
+++ react-app/src/components/Friend/userFriends.js
@@ -1,18 +1,25 @@
 import { Link } from 'react-router-dom';
 import './friend.css'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getUserFriendsThunk } from '../../store/user';
 
 export default function UserFriends({sessionUser}) {
     const userFriends = useSelector((state) => state.users.userFriends);
-    // console.log("user friends in userFriends component: ", userFriends);
+    console.log("user friends in userFriends component: ", userFriends);
     const userFriendsArr = Object.values(userFriends);
-    // console.log("user friend list in userFriends component: ", userFriendsArr);
-    // console.log("session user friend list in userFriends component: ", sessionUser.friends);
+    console.log("user friend list in userFriends component: ", userFriendsArr);
+    console.log("session user friend list in userFriends component: ", sessionUser.friends);
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getUserFriendsThunk(sessionUser.id))
+    }, [dispatch, sessionUser.id])
 
     
     return (
         <>
-        {userFriendsArr === 0 ? 
+        {userFriendsArr.length === 0 ? 
             <>
                 <h3>Contacts</h3>
                 <p>You don't have contacts now.</p>
